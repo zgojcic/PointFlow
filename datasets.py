6b35fb63a8db6657ae5cf3c9e09994a1bc01e7fb
zgojcic <zan.gojcic@gmail.com>
2022-05-12 13:58:20 +0200

    new cars

--- datasets.py
+++ datasets.py
@@ -26,7 +26,8 @@ synsetid_to_cate = {
     '04256520': 'sofa', '04330267': 'stove', '04530566': 'vessel',
     '04554684': 'washer', '02992529': 'cellphone',
     '02843684': 'birdhouse', '02871439': 'bookshelf',
-    '00000000': 'animal'
+    '00000000': 'animal',
+    '02958343_surface_pcd_all': 'cars_new'
     # '02834778': 'bicycle', not in our taxonomy
 }
 cate_to_synsetid = {v: k for k, v in synsetid_to_cate.items()}
